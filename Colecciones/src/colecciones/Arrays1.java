be36dbd412f1190d6a7b60ab88793c47fd10c945
karhox <victor170497@gmail.com>
2022-10-06 19:02:49 +0200

    tablas

--- Colecciones/src/colecciones/Arrays1.java
+++ Colecciones/src/colecciones/Arrays1.java
@@ -1,5 +1,6 @@
 package colecciones;
 
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
@@ -8,7 +9,6 @@ public class Arrays1 {
 
 		
 	public static void ejemplo1() {
-		int a;
 		int [] numeros = new int[5]; //de la posición 0 a la 4
 		int [] numeros2 = {6,4,3,8,5}; //Poner valores directamente
 		String[] dias = new String[7]; 
@@ -22,6 +22,7 @@ public class Arrays1 {
 		numeros[4] = 1;
 		//numeros[5] = 3; Error 
 		System.out.println("La suma da: "+ (numeros[1]+numeros[2]));
+		System.out.println(numeros2);//muestra como una id del array
 		
 		dias[0] = "lunes";
 		System.out.println(dias[1]); //Null -> No tiene un valor añadido
@@ -138,7 +139,7 @@ public class Arrays1 {
 
 	}	
 	
-	public static void CrearArrayApartirDeOtro() {
+	public static void crearArrayApartirDeOtro() {
 		
 		String[] meses = {"Enero","Febrero","Marzo","abril","Mayo", "Junio", "Julio", "Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
 		int [] letrasMeses = new int [meses.length]; //mismo tamaño que meses
@@ -151,10 +152,80 @@ public class Arrays1 {
 		}
 	}
 	
+	public static void arraysMultidimensionales() {
+		
+		//2 dimensiones -> tabla(matriz)
+		
+		// Ejemplo de tabla no escalonada
+		int[][] tabla = new int[4][3]; // crear el array y darle memoria
+		tabla[0][0] = 5;
+		tabla[0][1] = 4;
+		tabla[0][2] = 9;
+		tabla[1][0] = 8;
+		tabla[1][1] = 5;
+		tabla[1][2] = 1000;
+		tabla[2][0] = -2;
+		tabla[2][1] = 14;
+		tabla[2][2] = 5;
+		tabla[3][0] = 9;
+		tabla[3][1] = 0;
+		tabla[3][2] = 8;
+		int[][] tabla2 = { { 5, 4, 9 }, { 8, 5, 1000 }, { -2, 14, 5 }, { 9, 0, 8 } };
+
+		// recorrer las tablas
+		//int alto = tabla.length;
+		//int ancho = tabla[0].length;
+		for (int fila = 0; fila < tabla.length; fila++) {
+			// Mensajes antes de cada fila
+			//System.out.println("Tratamiento de la fila: " + fila);
+			for (int col = 0; col < tabla[fila].length; col++) {
+				System.out.print(tabla[fila][col] + " "); //IMPORTANTE AQUÍ PRINT (NO PRINTLN)
+			}
+			// Mensajes al final de cada fila
+			// System.out.println("\nFin de la fila: " + fila); // Generamos salto de línea
+			System.out.println();
+		}
+		// Mensajes al final de la tabla
+	}
+	public static void calculoNotasMedias() {
+		
+		int[][] notas = { { 5, 4, 9 }, { 8, 5, 10 }, { 6, 7, 5 }, { 9, 2, 8 } };
+		int numAlumno = 1;
+		double notaMediaGlobal =0;
+		for (int[] alumno: notas) {
+			//esto imprime antes de cada fila
+			double sumaNotas = 0;
+			for (int nota:alumno){
+				System.out.print(nota + " ");
+				sumaNotas += nota; // x = y; x+y = z; x+y+z
+			}
+			notaMediaGlobal += sumaNotas;
+			//esto imprime al final de cada fila
+			System.out.printf("Media del alumno %d: %.2f\n",numAlumno,(sumaNotas/alumno.length)); //longitud de cada fila del alumno 
+			numAlumno++; //Si se pone dentro del for se sumaria por cada nota
+		}
+		System.out.println("Numero de alumnos " + notas.length); //numero de filas
+		System.out.println("Numero de notas " + notas[0].length); //numero de columnas
+		System.out.printf("Media de la clase: %.2f\n",(notaMediaGlobal/notas.length*notas[0].length));
+	}
+	
+	public static void tablasMultiplicar() { //for anidado
+		for (int tabla = 1; tabla <=10; tabla++) {
+			System.out.println("Tabla del " + tabla);
+			for(int numero = 1; numero<=10; numero++) {
+				System.out.println(tabla + "*" + numero + "= " + (tabla*numero));
+			}
+			System.out.println("Fin de la tabla de "+tabla +"\n");
+		}
+	}
+	
 	public static void main(String[] args) {
 		//ejemplo1();
 		//ordenar();
-		CrearArrayApartirDeOtro();
+		//crearArrayApartirDeOtro();
+		//arraysMultidimensionales();
+		//calculoNotasMedias();
+		tablasMultiplicar();
 	}
 }
 
